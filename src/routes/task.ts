fee9620f49e05cf3160d4e6b4e106ea7a80a0865
Valera <v.drozdovich@intelico.su>
2021-01-16 17:03:03 +0300

    get tasks by category

--- src/routes/task.ts
+++ src/routes/task.ts
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 let Task = require('../models/task.model')
 
-type TaskType = {title: String, time: String, icon: String, id: String, categoryId: String}
+type TaskType = { title: String, time: String, icon: String, id: String, categoryId: String }
 
 type RequestType = {
-    body: TaskType
+    body: TaskType,
+    params: TaskType
 };
 
 type ResponceType = {
@@ -12,13 +13,13 @@ type ResponceType = {
     json: (value: Array<TaskType> | String) => TaskType
 };
 
-router.route('/').get((_req: RequestType, res:ResponceType) => {
+router.route('/').get((_req: RequestType, res: ResponceType) => {
     Task.find()
-        .then((tasks:Array<TaskType>) => res.json(tasks))
-        .catch((err:String) => res.status(400).json(err))
+        .then((tasks: Array<TaskType>) => res.json(tasks))
+        .catch((err: String) => res.status(400).json(err))
 });
 
-router.route('/add').post((req: RequestType, res:ResponceType) => {
+router.route('/add').post((req: RequestType, res: ResponceType) => {
     const title = req.body.title
     const time = req.body.time
     const icon = req.body.icon
@@ -33,7 +34,18 @@ router.route('/add').post((req: RequestType, res:ResponceType) => {
 
     newTask.save()
         .then(() => res.json('Task added'))
-        .catch((err:String) => res.status(400).json(err))
+        .catch((err: String) => res.status(400).json(err))
 });
+
+router.route('/by-category/:categoryId').get((req: RequestType, res: ResponceType) => {
+    const categoryId = req.body.categoryId
+
+    Task.find()
+        .then((tasks: Array<TaskType>) => {
+            const filteredTasks = tasks.filter(e => e.categoryId === categoryId)
+            res.json(filteredTasks)
+        })
+        .catch((err: String) => res.status(400).json(err))
+})
 
 module.exports = router;
